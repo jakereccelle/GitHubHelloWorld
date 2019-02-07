4f0759b627c7b400444f5e051e8fb164cd40a271
Jake Reccelle <jcr21@students.ptcollege.edu>
2019-02-07 11:44:44 -0500

    this is the first commit

---
+++ src/githubhelloworld/GitHubHelloWorld.java
@@ -0,0 +1,11 @@
+package githubhelloworld;
+
+public class GitHubHelloWorld {
+
+    public static void main(String[] args) {
+        Reccelle();
+    }
+    public static void Reccelle(){
+        System.out.println("Jake Reccelle!");
+    }
+}
